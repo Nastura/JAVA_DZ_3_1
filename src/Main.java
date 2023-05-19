2a23af1338ecd5e6a7c01420fd6fb1c6159d8f7e
Nastura <chulmost@gmail.com>
2023-05-19 23:53:49 +0300

    update file

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
-
         int price;
-        int Mili;
+        int mili;
         price = 13676;
 
         if (price > 20) {
-            Mili = price / 20;
+            mili = price / 20;
         } else {
-            Mili = 0;
+            mili = 0;
         }
 
         System.out.println("Стоимость билета: " + price);
-        System.out.println("Начисленные мили: " + Mili);
-
-
-
+        System.out.println("Начисленные мили: " + mili);
     }
 }
